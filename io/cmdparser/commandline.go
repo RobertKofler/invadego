28fce89040385715ac6019d81aef6c09771b032c
Robert Kofler <rokofler@pgnmc008.vu-wien.ac.at>
2024-12-10 13:55:22 +0100

    new feature base popualtion all:10'

--- io/cmdparser/commandline.go
+++ io/cmdparser/commandline.go
@@ -47,7 +47,7 @@ func ParseCommandLine() *CommandLineParameters {
 	episilence := flag.String("epi-inherit", "", "mandatory;  either dros, ara, none")
 	genome := flag.String("genome", "", "mandatory; the genomic landscape; e.g. 'MB:2,3,1,5' specifiies four chromosomes with sizes of 2,3,1,5 Mb")
 	generations := flag.Int64("gen", -1, "mandatory; run the simulations for '--gen' generations")
-	basepop := flag.String("basepop", "", "mandatory; the individual(s) with the segregating insertions in the starting population; CoordY,CoordX,N:CoordY,CoordX,N; where 10' menas 10 silenced insertions")
+	basepop := flag.String("basepop", "", "mandatory; the individual(s) with the segregating insertions in the starting population; CoordY,CoordX,N:CoordY,CoordX,N; where 10' menas 10 silenced insertions; can also be of the form all:10' where all receive the same initiatl TEs ")
 
 	// Optional parameters
 	triggerSilence := flag.Int64("trigger-defense", -1, "at which threshold value should the host defense been triggerd")
--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main_profile() {
 func main() {
 
 	// VERSION NUMBER
-	version := "0.1.3"
+	version := "0.1.4"
 
 	clp := cmdparser.ParseCommandLine()
 	if clp.Silent {
